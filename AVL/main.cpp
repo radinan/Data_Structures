39b74fa81cd8df44cdf83b06c0a4e45b1375c457
Radina <56393689+radinan@users.noreply.github.com>
2021-01-04 22:10:49 +0200

    static attribute
    Adding static attribute, so the tree can be sorted by different criteria.

--- AVL/main.cpp
+++ AVL/main.cpp
@@ -1,108 +1,114 @@
 #include <iostream>
-#include "AVLTree.h"
+#include "AVL.h"
 #include <string>
 
 
 enum class Property{name, artist, year};
 
-class Song //test class for my project
+class Song
 {
 private:
 	std::string name;
 	std::string artist;
-	int year;
-	Property property = Property::name; //by default
-
+	size_t year;
+public:
+	static Property property; //one for all objects
 public:
 	Song(const std::string& name, const std::string& artist, int year) : name(name), artist(artist), year(year) {}
-	void set(const Property& _property) 
-	{
-		property = _property;
-	}
 
-	friend bool operator>(const Song& one, const Song& other) //without friend
+	Song& operator=(const Song& other)
 	{
-		switch (one.property)
+		if (this != &other)
+		{
+			name = other.name;
+			artist = other.artist;
+			year = other.year;
+		}
+		return *this;
+	}
+	
+	friend bool operator>(const Song& left, const Song& right) //without friend
+	{
+		switch (Song::property)
 		{
 		case Property::artist:
-			return one.artist > other.artist;
+			return left.artist > right.artist;
 			break;
 		case Property::name:
-			return one.name > other.name;
+			return left.name > right.name;
 			break;
 		case Property::year:
-			return one.year > other.year;
+			return left.year > right.year;
 			break;
 		}
 	}
-	friend bool operator<(const Song& one, const Song& other) //without friend
+	friend bool operator<(const Song& left, const Song& right)
 	{
-		switch (one.property)
+		switch (Song::property)
 		{
 		case Property::artist:
-			return one.artist < other.artist;
+			return left.artist < right.artist;
 			break;
 		case Property::name:
-			return one.name < other.name;
+			return left.name < right.name;
 			break;
 		case Property::year:
-			return one.year < other.year;
+			return left.year < right.year;
 			break;
 		}
 	}
-	friend bool operator==(const Song& one, const Song& other) //without friend
+	friend bool operator==(const Song& left, const Song& right) 
 	{
-		switch (one.property)
+		switch (Song::property)
 		{
 		case Property::artist:
-			return one.artist == other.artist;
+			return left.artist == right.artist;
 			break;
 		case Property::name:
-			return one.name == other.name;
+			return left.name == right.name;
 			break;
 		case Property::year:
-			return one.year == other.year;
+			return left.year == right.year;
 			break;
 		}
 	}
 	friend std::ostream& operator<<(std::ostream& out, Song& one)
 	{
-		out << one.name << " " << one.year << " ";// << std::endl;
+		out << one.name << " " << one.artist << " " << one.year << " ";// << std::endl;
 		return out;
 	}
-};
 
+};
+Property Song::property = Property::year; //by default
 
 int main()
 {
-	Song s("i","c", 9), s1("h","b", 5), s2("g","a", 10), s3("f","b", 0), s4("e","f",6), s5("d", "c", 11),
-		s6("c", "c", -1), s7("b", "c", 1), s8("a", "c", 2);
-	s.set(Property::year);
-	s1.set(Property::year);
-	s2.set(Property::year);
-	s3.set(Property::year);
-	s4.set(Property::year);
-	s5.set(Property::year);
-	s6.set(Property::year);
-	s7.set(Property::year);
-	s8.set(Property::year);
+	Song s("a", "c", 9), s1("b", "b", 5), s2("c", "a", 11);
 
-	AVL<Song> rr;
-	rr.insert(s);
-	rr.insert(s1);
-	rr.insert(s2);
-	rr.insert(s3);
-	rr.insert(s4);
-	rr.insert(s5);
-	rr.insert(s6);
-	rr.insert(s7);
-	rr.insert(s8);
-	std::cout << "Preorder traversal of the given tree \n";
-	rr.preorder();
+	//--year--
+	AVL<Song> year_sorted;
+	year_sorted.insert(s);
+	year_sorted.insert(s1);
+	year_sorted.insert(s2);
+	year_sorted.inorder();
+	std::cout << std::endl;
 
-	rr.remove(s2);
-	std::cout << "Preorder traversal of the given tree \n";
-	rr.preorder();
-  
-  return 0;
-  }
+	//--name--
+	Song::property = Property::name;
+
+	AVL<Song> name_sorted;
+	name_sorted.copy_insert(year_sorted);
+	name_sorted.inorder();
+	std::cout << std::endl;
+
+	//--artist--
+	Song::property = Property::artist;
+
+	AVL<Song> artist_sorted;
+	artist_sorted.copy_insert(name_sorted);
+	artist_sorted.inorder();
+	std::cout << std::endl;
+
+
+	return 0;
+}
