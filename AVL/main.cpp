abbaedecc10e02272cc3bb177d0e508c354f9307
Radina <56393689+radinan@users.noreply.github.com>
2021-01-05 21:44:51 +0200

    updating operators overload
    updating the comparison criteria, so the tree won't take 2 different Song objects as equal

--- AVL/main.cpp
+++ AVL/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 
-enum class Property{name, artist, year};
+enum class Property { name, artist, year };
 
 class Song
 {
@@ -26,18 +26,50 @@ public:
 		}
 		return *this;
 	}
-	
+
 	friend bool operator>(const Song& left, const Song& right) //without friend
 	{
+		//name,artist,year
 		switch (Song::property)
 		{
-		case Property::artist:
-			return left.artist > right.artist;
-			break;
 		case Property::name:
+			if (left.name == right.name)
+			{
+				if (left.artist == left.artist)
+				{
+					return left.year > right.year;
+					break;
+				}
+				return left.artist > right.artist;
+				break;
+			}
 			return left.name > right.name;
 			break;
+		case Property::artist:
+			if (left.artist == right.artist)
+			{
+				if (left.name == left.name)
+				{
+					return left.year > right.year;
+					break;
+				}
+				return left.name > right.name;
+				break;
+			}
+			return left.artist > right.artist;
+			break;
+
 		case Property::year:
+			if (left.year == right.year)
+			{
+				if (left.name == left.name)
+				{
+					return left.artist > right.artist;
+					break;
+				}
+				return left.name > right.name;
+				break;
+			}
 			return left.year > right.year;
 			break;
 		}
@@ -46,18 +78,49 @@ public:
 	{
 		switch (Song::property)
 		{
-		case Property::artist:
-			return left.artist < right.artist;
-			break;
 		case Property::name:
+			if (left.name == right.name)
+			{
+				if (left.artist == left.artist)
+				{
+					return left.year < right.year;
+					break;
+				}
+				return left.artist < right.artist;
+				break;
+			}
 			return left.name < right.name;
 			break;
+		case Property::artist:
+			if (left.artist == right.artist)
+			{
+				if (left.name == left.name)
+				{
+					return left.year < right.year;
+					break;
+				}
+				return left.name < right.name;
+				break;
+			}
+			return left.artist < right.artist;
+			break;
+
 		case Property::year:
+			if (left.year == right.year)
+			{
+				if (left.name == left.name)
+				{
+					return left.artist < right.artist;
+					break;
+				}
+				return left.name < right.name;
+				break;
+			}
 			return left.year < right.year;
 			break;
 		}
 	}
-	friend bool operator==(const Song& left, const Song& right) 
+	friend bool operator==(const Song& left, const Song& right)
 	{
 		switch (Song::property)
 		{
@@ -79,11 +142,11 @@ public:
 	}
 
 };
-Property Song::property = Property::year; //by default
+Property Song::property = Property::name; //by default
 
 int main()
 {
-	Song s("a", "c", 9), s1("b", "b", 5), s2("c", "a", 11);
+	Song s("b", "c", 9), s1("b", "b", 5), s2("c", "a", 11);
 
 	//--year--
 	AVL<Song> year_sorted;
@@ -96,16 +159,14 @@ int main()
 	//--name--
 	Song::property = Property::name;
 
-	AVL<Song> name_sorted;
-	name_sorted.copy_insert(year_sorted);
+	AVL<Song> name_sorted(year_sorted);
 	name_sorted.inorder();
 	std::cout << std::endl;
 
 	//--artist--
 	Song::property = Property::artist;
 
-	AVL<Song> artist_sorted;
-	artist_sorted.copy_insert(name_sorted);
+	AVL<Song> artist_sorted(year_sorted);
 	artist_sorted.inorder();
 	std::cout << std::endl;
 
